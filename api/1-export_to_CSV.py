ea20208b01c0d6677c754848da0f13bdecf24ee2
Paradis007-bit <c.hirwa2@alustudents.com>
2026-03-12 15:04:47 +0000

    Task 1: PEP8-compliant CSV export script

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-"""Export employee TODO list to CSV file."""
+"""Export employee TODO list to CSV file for a given employee ID."""
 
 import csv
 import requests
 import sys
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        usage = "Usage: {} <employee_id>".format(sys.argv[0])
+        print(usage)
         sys.exit(1)
 
     emp_id = sys.argv[1]
 
     # Fetch user data
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
+    user_url = (
+        "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
+    )
     user_res = requests.get(user_url).json()
     username = user_res.get("username")
 
     # Fetch TODOs
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(emp_id)
+    todos_url = (
+        "https://jsonplaceholder.typicode.com/todos?userId={}"
+        .format(emp_id)
+    )
     todos = requests.get(todos_url).json()
 
     # CSV filename
     filename = "{}.csv".format(emp_id)
 
-    # Write to CSV
+    # Write tasks to CSV
     with open(filename, mode="w", newline="") as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for task in todos:
